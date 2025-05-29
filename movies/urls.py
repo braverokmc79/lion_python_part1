e24eee4d8ef022d42fa94ce13fa24b3793d0391f
braverokmc79 <braverokmc79@gmail.com>
2025-05-29 17:11:57 +0900

    a,b,c,d,e 페이지

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),    
+    path('movies/', views.movie_list, name='movies'),
+    path("movie/<int:movie_id>/", views.movie_detail, name="movie_detail"),
+    
+    path('page/<str:id>/', views.str_page, name='str_page'),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html')
+    return render(request, 'index.html')
+
+
+def movie_list(request):
+    return render(request, 'movies/movie_list.html')
+
+
+
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+
+
+def str_page(request, id):
+    return render(request, 'movies/str_page.html', {'id': id})
+
+
+
+
+
+
+
+
